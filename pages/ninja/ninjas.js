13afbdba08df4b3c6d34dbf2ec911f23657cf5c3
daniwidodo <dani.widodo87@gmail.com>
2022-06-23 04:33:54 +0700

    add fetch and fix styling

--- pages/ninja/ninjas.js
+++ pages/ninja/ninjas.js
@@ -1,9 +1,31 @@
-const Ninjas = () => {
-    return ( 
-        <div>
-            <h1>Hi Ninjas!</h1>
+import { useEffect } from "react";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
+//   useEffect( () => {
+//     console.log(ninjas);
+//   }, [])
+  return (
+    <div>
+      <h1>Hi Ninjas!</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a>
+            <h4>{ninja.name}</h4>
+          </a>
         </div>
-     );
-}
- 
+      ))}
+    </div>
+  );
+};
+
+
 export default Ninjas;
